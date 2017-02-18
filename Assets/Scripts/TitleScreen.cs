5502c70c5ec9289e7a8a69df89e1ed48cba95023
Ryan Rose <ryanthrose@gmail.com>
2017-02-18 15:43:18 -0600

    Added a title screen.

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour {
+
+	public void LoadGame() {
+		Application.LoadLevel ("Dropping");
+	}
+}
